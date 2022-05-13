38c621cee4a3313fd96ed15f7bd8e5b5fafa0961
cjiping <cjiping@linewell.com>
2022-05-13 14:31:11 +0800

    缓存支持

--- main.js
+++ main.js
@@ -2,21 +2,31 @@ const googleTrends = require('google-trends-api');
 const HttpsProxyAgent = require('https-proxy-agent');
 var http = require('http')
 const url = require('url');
+const NodeCache = require("node-cache");
+const cache = new NodeCache();
 
-let proxyAgent =  new HttpsProxyAgent('http://127.0.0.1:54088/');
+let proxyAgent = new HttpsProxyAgent('http://127.0.0.1:54088/');
 
-http.createServer(async function(request,response){
+http.createServer(async function (request, response) {
   request = url.parse(request.url, true);
   const methodName = request.pathname.replace("/", '')
-  console.log(request)
-  let query = {
-      keyword: request.query.kw,
-      agent: proxyAgent
-  };
+  const kw = request.query.kw
+  let data;
+  const cacheKey = methodName + "-" + kw
   try {
+    if (!cache.get(cacheKey)) {
+      let query = {
+        keyword: kw,
+        agent: proxyAgent
+      };
+      data = await googleTrends[methodName](query)
+      cache.set(cacheKey, data, 86400)
+    } else {
+      data = cache.get(cacheKey)
+    }
     response.setHeader("Content-Type", "application/json;charset=utf-8")
-    response.end(await googleTrends[methodName](query));
-  }catch(err) {
+    response.end(data);
+  } catch (err) {
     response.end(err.toString());
   }
 }).listen(28080);
